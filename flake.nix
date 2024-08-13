3485f842c30c0c799c83231611dd7142f668abe9
riweston <github@riweston.io>
2024-08-13 10:51:48 +0100

    fix: This module links up home-manager programs as Mac Apps for spotlight

--- flake.nix
+++ flake.nix
@@ -12,19 +12,26 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nh_darwin.url = "github:ToyVo/nh_darwin";
+    # Temp fix until https://github.com/nix-community/home-manager/issues/1341 is resolved
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, darwin, nh_darwin, ... }:
-    let
-
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    home-manager,
+    darwin,
+    nh_darwin,
+    mac-app-util,
+    ...
+  }: let
       hostname = "MAC-EVIDEN-NQK9H7JC54";
       system = "aarch64-darwin";
 
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-      };
-    in {
+      }; in {
       darwinConfigurations."${hostname}" = darwin.lib.darwinSystem {
         inherit system;
         specialArgs = inputs;
@@ -36,6 +43,11 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.riweston = import ./home;
+            home-manager.sharedModules = [
+              # This links up home-manager enabled programs with Mac Applications
+              # so that they can be opened from the Dock or Spotlight
+              mac-app-util.homeManagerModules.default
+            ];
           }
         ];
       };
