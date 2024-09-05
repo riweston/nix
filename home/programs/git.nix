c96621a12a5ddb9a182e17352b373508673ed8dc
riweston <github@riweston.io>
2024-09-05 17:14:01 +0100

    feat: Add a handy global gitignore generator

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,11 +1,32 @@
-{
+{ config, lib, pkgs, ... }:
+let
+  gitignoreTemplates =
+    [ "macos" "windows" "linux" "visualstudiocode" "jetbrains+all" ];
+
+  # Join the templates into a comma-separated string
+  templateString = builtins.concatStringsSep "," gitignoreTemplates;
+
+  # Fetch gitignore content from Toptal API using curl
+  gitignoreContent = builtins.readFile
+    (pkgs.runCommand "gitignore-content" { } ''
+      ${pkgs.curl}/bin/curl -k -L "https://www.toptal.com/developers/gitignore/api/${templateString}" > $out
+    '');
+
+  # Split the content into lines and filter out empty lines
+  gitIgnoreList =
+    lib.filter (line: line != "") (lib.splitString "\n" gitignoreContent);
+
+in {
   programs.git = {
     enable = true;
     userEmail = "github@riweston.io";
     userName = "riweston";
     extraConfig = {
       init.defaultBranch = "main";
-      "url \"git@github.com:\"".insteadOf = "https://github.com";
+      url = {
+        "ssh://git@github.com/" = { insteadOf = "https://github.com/"; };
+      };
     };
+    ignores = gitIgnoreList;
   };
 }
